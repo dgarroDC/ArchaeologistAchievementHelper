e69cc0fa6d765084142129e4fd0dc69ab6f14767
dgarro <dgarro@dc.uba.ar>
2022-01-16 20:09:09 -0300

    initial commit

---
+++ ArchaeologistAchievementHelper/ArchaeologistAchievementHelper.cs
@@ -0,0 +1,61 @@
+﻿using OWML.ModHelper;
+using OWML.Common;
+
+namespace ArchaeologistAchievementHelper
+{
+    public class ArchaeologistAchievementHelper: ModBehaviour
+    {
+        private static bool _enabled;
+        private static bool _showMissingFacts;
+
+        private void Start()
+        {
+            ModHelper.HarmonyHelper.AddPostfix<ShipLogEntry>("HasMoreToExplore", typeof(ArchaeologistAchievementHelper), nameof(ArchaeologistAchievementHelper.ShipLogEntryHasMoreToExplore));
+            ModHelper.HarmonyHelper.AddPostfix<ShipLogEntryDescriptionField>("SetEntry", typeof(ArchaeologistAchievementHelper), nameof(ArchaeologistAchievementHelper.ShipLogEntryDescriptionFieldSetEntry));
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            _enabled = config.Enabled;
+            _showMissingFacts = config.GetSettingsValue<bool>("Show missing facts (WARNING: SPOILERS)");
+        }
+
+        private static void ShipLogEntryHasMoreToExplore(ShipLogEntry __instance, ref bool __result)
+        {
+            if (_enabled && !__result && __instance.GetState() == ShipLogEntry.State.Explored)
+            {
+                foreach (ShipLogFact fact in __instance.GetExploreFacts())
+                {
+                    if (IsMissingForArcheologhistAchievement(__instance, fact))
+                    {
+                        __result = true;
+                    }
+                }
+            }
+        }
+
+        private static void ShipLogEntryDescriptionFieldSetEntry(ShipLogEntryDescriptionField __instance, ShipLogEntry entry)
+        {
+            if (_enabled && _showMissingFacts && entry.GetState() == ShipLogEntry.State.Explored)
+            {
+                foreach (ShipLogFact fact in entry.GetExploreFacts())
+                {
+                    if (IsMissingForArcheologhistAchievement(entry, fact))
+                    {
+                        // There are enough items (10), entries have up to 6 explore facts, so we need max 7 items
+                        __instance._factListItems[__instance._displayCount].DisplayText($"<color=orange>MISSING FACT: {fact.GetText()}</color>");
+                        __instance._displayCount++;
+                    }
+                }
+            }
+        }
+
+        // This is based on ShipLogManager.CheckForCompletionAchievement()
+        private static bool IsMissingForArcheologhistAchievement(ShipLogEntry entry, ShipLogFact fact)
+        {
+            return !fact.IsRumor() && !fact.IsRevealed() &&
+                !fact.GetID().Equals("TH_VILLAGE_X3") && !fact.GetID().Equals("GD_GABBRO_ISLAND_X1") &&
+                entry.GetCuriosityName() != CuriosityName.InvisiblePlanet;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Archaeologist Achievement Helper by Damián Garro
+
+![thumbnail](images/thumbnail.png)
+
+Mark ship log entries that have missing facts required for the Archaeologist achievement as *"There's more to explore here."*. It also lets you view which facts you haven't revealed!
+
+## Basic usage
+
+In Outer Wilds, when an entry **isn't** displayed as *"There's more to explore here."* it doesn't mean that all of its facts are revealed, there are some facts that don't affect that mark, and some of them are even required for the Archaeologist achievement. For example, the Timber Hearth Village entry has 2 facts that are needed for the achievement, but this entry is never marked as *"There's more to explore here."*:
+
+![example-disabled](images/example-disabled.jpg)
+
+This makes it difficult to know where to find the facts that you are missing for the achievement. The Archaeologist Achievement Helper mod forces *"There's more to explore here."* to appear if there is at least one unrevealed fact that is required for Archaeologist:
+
+![example-enabled](images/example-enabled.jpg)
+
+This feature helps you know in which entries there may be missing facts that you need for the achievement.
+
+## Show all missing facts feature
+
+If you need even more help, you can activate a setting to show all unrevealed facts that Archaeologist requires:
+
+![settings](images/settings.png)
+
+When you activate it the missing facts are shown like this (below the *"There's more to explore here."* text, with the same color and starting with *"MISSING FACT: "*):
+
+![example-disabled](images/example-showall.jpg)
+
+**Beware of spoilers**, only activate this option if you already beat the game, as this could reveal something important you don't know, although if you are using this mod you probably won the game and you are chasing the achievements.
+
+If even with this option you are still unable to find the missing facts, I suggest going to the [Outer Wilds unofficial Discord server](https://discord.gg/kChJgBMmtd) and asking for help in `#archaeologist-achievement` channel (please remember to let them know which facts you have missing).
